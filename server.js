0c9afa7c960738be6644f2dfa5dbdd15328fe2c1
benkigano <benkigano@gmail.com>
2026-01-18 09:29:19 -0800

    Update server.js

--- server.js
+++ server.js
@@ -51,16 +51,16 @@ app.get("/test-email", (req, res) => {
 
   (async () => {
     try {
-      const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user: process.env.GMAIL_USER,
-          pass: process.env.GMAIL_APP_PASSWORD,
-        },
-        connectionTimeout: 10000,
-        greetingTimeout: 10000,
-        socketTimeout: 10000,
-      });
+    const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_APP_PASSWORD,
+  },
+});
+ 
 
       await transporter.sendMail({
         from: process.env.GMAIL_USER,
