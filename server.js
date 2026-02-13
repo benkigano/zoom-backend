e2f1d9209662693f9dedfb44c4a18cf76024771b
benkigano <benkigano@gmail.com>
2026-02-13 14:50:27 -0800

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
   console.log("➡️", req.method, req.originalUrl);
   next();
 });
-const zoomTokenStore = new Map();
+
 
 app.use((req, res, next) => {
   if (req.originalUrl === "/zoom/webhook") {
